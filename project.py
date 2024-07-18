7964498eff7ef44afa7c497ecd68fead94c00789
Haniasaad19 <hani.as.asaad@gmail.com>
2024-07-18 10:36:52 -0400

    adding user

--- project.py
+++ project.py
@@ -2,11 +2,18 @@ class user:
     def __init__(self,user_id,name):
         self.user_id=user_id
         self.name=name
-        self.friends=set()
+        self.friends=[]
     def add_friend(self,friend):
-        self.friends.add(friend)
+        self.friends.append(friend)
     def remove_friend(self,friend):
-        self.friends.discard(friend)
+        self.friends.remove(friend)
 class SocialNetworkGraph:
     def __init__(self):
         self.users={}
+    def add_user(self,user):
+        if user not in self.users:
+            self.users[user]=[]
+        else:
+            print('this user is already using the network ')
+            
+    
